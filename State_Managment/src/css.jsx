8ffc1db58c88c24fbb7d0a30eb5218f4c1a80f9f
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-10-17 12:39:32 +0530

    Oct17

--- State_Managment/src/css.jsx
+++ State_Managment/src/css.jsx
@@ -6,10 +6,11 @@ const Css = () => {
     padding: "10px",
     borderRadius: "5px",
   };
+  //static Inline CSS
   function Greeting() {
     return <h2 style={style}> Hello inline</h2>;
   }
-
+  // Dynamic Inline CSS
   function Temperature({ value }) {
     return (
         <p style={{color:value>30?"red":"blue"}}>Temp : {value}°C</p>
